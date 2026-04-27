8277d4d06e39c5894dd88f90d00c303e701c6c39
Henry Zhang <zhang769641@sas.edu.sg>
2026-04-27 14:47:38 +0800

    dum

--- scenes/characters/dumbert_2_legs.gd
+++ scenes/characters/dumbert_2_legs.gd
@@ -1,16 +1,30 @@
 extends "res://scenes/characters/dumbert.gd"
 
-@export var max_speed = 600
-@export var jump_speed = 1000
+@export var max_speed = 20
+var accel = 10
+@export var jump_speed = 2000
+
+var dirFacing = 1
+var player_velocity = Vector2(0, 0)
+var player_velocity_last_frame = Vector2(0, 0)
 
 func _physics_process(delta: float) -> void:
 	if !get_tree().paused:
 		collectItems()
 		var x = Input.get_axis("left", "right")
-		linear_velocity.x = x * max_speed
+		if x:
+			dirFacing = x
+		else:
+			player_velocity.x = 0
+		if player_velocity.length() < max_speed:
+			player_velocity += Vector2(x * accel, 0)
+		else:
+			player_velocity.x = max_speed * sign(player_velocity.x)
 		if is_on_floor():
 			if Input.is_action_just_pressed("space"):
 				apply_impulse(Vector2(0, -jump_speed))
+		
+		global_position += player_velocity
 
 func is_on_floor():
 	#print($Area2D.get_overlapping_bodies())
